4ec29a43b93a32040222d6c189622e4f34eaa6a1
LucasMedici <lucasrpmedici@gmail.com>
2024-06-20 09:09:35 -0300

    log de alidacao

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -18,6 +18,7 @@ router.post('/register', async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Erro ao registrar usuário' });
+        logger.info('Erro ao registrar usuario na rota (/register)')
     }
 });
 
@@ -28,10 +29,13 @@ router.post('/login', async (req, res) => {
     try {
         const user = await User.findOne({ where: { username } });
         if (!user) {
+            logger.info('Usuário não encontrado ao logar')
             return res.status(404).json({ error: 'Usuário não encontrado' });
+            
         }
         const isPasswordValid = await bcrypt.compare(password, user.password);
         if (!isPasswordValid) {
+            logger.info('Credenciais inválidas')
             return res.status(401).json({ error: 'Credenciais inválidas' });
         }
         const token = jwt.sign({ id: user.id, username: user.username, role: user.role }, 'secreto', { expiresIn: '1h' });
